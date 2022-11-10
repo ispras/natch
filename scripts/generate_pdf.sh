34f77991b16f00bd73e06fec4e6b573c3155c872
Natalia Fursova <notawa@users.noreply.github.com>
2022-11-10 17:29:56 +0300

    Update generate_pdf.sh

--- scripts/generate_pdf.sh
+++ scripts/generate_pdf.sh
@@ -5,12 +5,12 @@ SCRIPTS_DIR="$(dirname $(readlink -e "$0"))"
 $SCRIPTS_DIR/preparation.py
 
 pandoc -B $SCRIPTS_DIR/titul.md $SCRIPTS_DIR/toc.md -V colorlinks --css=$SCRIPTS_DIR/style.css $SCRIPTS_DIR/../[^Rt]*.md -o natch_docs.pdf \
-		--pdf-engine=weasyprint \
+	--pdf-engine=weasyprint \
        	--metadata pagetitle="Natch documentation" \
-		--metadata lang="Ru" \
-#		--pdf-engine-opt="-Oimages" \
-#		--verbose > gen.html \
-#		-N
+	--metadata lang="Ru" \
+#	--pdf-engine-opt="-Oimages" \
+#	--verbose > gen.html \
+#	-N
 
 if [[ -d $SCRIPTS_DIR/../.git ]]; then
 	cd $SCRIPTS_DIR/..

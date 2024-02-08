010127ca0c7e1938be6604c93e7f85b3c5ff5e0e
notawa <withoutacetone@gmail.com>
2024-02-08 17:43:09 +0300

    new documentation. not ready yet!

--- scripts/generate_pdf.sh
+++ scripts/generate_pdf.sh
@@ -2,18 +2,43 @@
 
 SCRIPTS_DIR="$(dirname $(readlink -e "$0"))"
 
-$SCRIPTS_DIR/preparation.py
+#$SCRIPTS_DIR/preparation.py
 
-pandoc -B $SCRIPTS_DIR/titul.md $SCRIPTS_DIR/toc.md -V colorlinks --css=$SCRIPTS_DIR/style.css $SCRIPTS_DIR/../[^Rt]*.md -o natch_docs.pdf \
-	--pdf-engine=weasyprint \
-       	--metadata pagetitle="Natch documentation" \
-	--metadata lang="Ru" \
-#	--pdf-engine-opt="-Oimages" \
-#	--verbose > gen.html \
-#	-N
+CUR_DIR=$(pwd)
+cd $SCRIPTS_DIR/..
 
-if [[ -d $SCRIPTS_DIR/../.git ]]; then
-	cd $SCRIPTS_DIR/..
-	git reset --hard > /dev/null
-fi
+pandoc -B scripts/titul.md scripts/toc.md -V colorlinks --css=scripts/style.css \
+		1_natch.md \
+		2_setup.md \
+		3_quickstart.md \
+		4_setup_env.md \
+		5_create_project.md \
+		6_taint_source.md \
+		7_scenario_work.md \
+		8_snatch.md \
+		9_additional.md \
+		10_automation.md \
+		11_utils.md \
+		12_applications.md \
+		13_faq.md \
+		14_app_license.md \
+		15_app_qemu_cmdline.md \
+		16_app_configs.md \
+		17_app_module_cfg.md \
+		18_app_coverage.md \
+		19_app_natch_cmds.md \
+		20_app_requirements.md \
+		21_app_oo_preparation.md \
+		22_app_releases.md \
+		-o $CUR_DIR/natch_docs.pdf \
+        --pdf-engine=weasyprint \
+        --metadata pagetitle="Natch documentation" \
+        --metadata lang="Ru" \
+#       --verbose > gen.html \
+#       -N
+
+#if [[ -d .git ]]; then
+#       git reset --hard > /dev/null
+#fi
+
 

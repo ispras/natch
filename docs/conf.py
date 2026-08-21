da104fef9941752332d045535ec45daf476aa52f
notawa <withoutacetone@gmail.com>
2026-08-21 15:25:51 +0300

    logo and content width

--- docs/conf.py
+++ docs/conf.py
@@ -34,6 +34,7 @@ myst_heading_anchors = 3
 
 templates_path = ["_templates"]
 html_static_path = ["_static"]
+html_css_files = ["custom.css"]
 
 exclude_patterns = [
     "_build",
@@ -45,6 +46,7 @@ exclude_patterns = [
 
 html_theme = "sphinx_rtd_theme"
 html_title = "Natch documentation"
+html_logo  = "_static/logo.png"
 
 # Show "View page source" in the upper-right corner.
 
